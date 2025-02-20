6818f59abb4446fc4df6af1f9de7b7c033af0547
DaPanda03 <77337331+DaPanda03@users.noreply.github.com>
2025-02-19 20:31:00 -0800

    trying to delpy to github pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/kwo-website/', // Adjust base URL to the subpath for GitHub Pages
   plugins: [react()],
+  resolve: {
+    alias: {
+      crypto: 'crypto-browserify',
+    },
+  },
+  define: {
+    'process.env': {},
+    global: 'window',
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    target: 'esnext',
+  },
+  server: {
+    port: 3000,
+  }
 })
